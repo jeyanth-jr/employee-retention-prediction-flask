a73839a7db42b29ed92add22a5d31eea34b23e98
Jeyanth <66862827+jeyanth-jr@users.noreply.github.com>
2023-01-08 10:24:42 +0530

    Update retention.py

--- retention.py
+++ retention.py
@@ -7,21 +7,19 @@ import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 import warnings
 import pickle
+from sklearn import metrics
 warnings.filterwarnings("ignore")
 labelencoder = LabelEncoder()
 log_reg = LogisticRegression()
 data = pd.read_csv(r"C:\Users\Jeyanth\Downloads\IBM Attrition Data.csv")
 data['Attrition'] = labelencoder.fit_transform(data['Attrition'])
-data['Department'] = labelencoder.fit_transform(data['Department'])
-data['EducationField'] = labelencoder.fit_transform(data['EducationField'])
-data['MaritalStatus'] = labelencoder.fit_transform(data['MaritalStatus'])
 x = data.drop(columns=["Attrition"])
 x = x.drop(columns=["Department"])
 x = x.drop(columns=["EducationField"])
 x = x.drop(columns=["MaritalStatus"])
 y = data.Attrition
 x_train, x_test, y_train, y_test = train_test_split(
-    x, y, train_size=.8, random_state=42)
+    x, y, train_size=.75, random_state=42)
 log_reg.fit(x_train, y_train)
 pickle.dump(log_reg, open('model.pkl', 'wb'))
 model = pickle.load(open('model.pkl', 'rb'))
